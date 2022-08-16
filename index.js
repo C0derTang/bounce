34060e6bbefff47350f533c805f70b9a35fb82b1
C0derTang <67521455+C0derTang@users.noreply.github.com>
2022-08-16 12:22:31 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,143 @@
+const express = require('express');
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+app.get('/',function(req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
+
+app.use('/',express.static(__dirname));
+
+/* Server */
+// Movement keys
+const W = 87;
+const A = 65;
+const S = 83;
+const D = 68;
+
+// The data
+let data = {players:{}} ;
+
+let width = 1200;
+let height = 600;
+
+io.sockets.on("connection", socket => {
+
+  // New player connected
+  console.log("player connected");
+
+  // Create new player
+  data.players[socket.id] = {
+    pos:{x:Math.random() * ((width-30) - 30 +1) + 30, y:Math.random() * ((height-30) - 30 +1) + 30},
+    vel:{xv:0, yv:0},
+    mass:1,
+    size:30,
+    color:{r:Math.random()*255, g:Math.random()*255, b:Math.random()*255}
+  };
+    io.emit('update', data);
+console.log(data);
+
+  socket.on("disconnect", () => {
+    // Player  disconnected
+    console.log("player disconnected");
+    // Delete player
+    delete data.players[socket.id];
+  });
+
+setInterval(function(){
+    if (socket.id in data.players){
+        let p = data.players[socket.id]
+        let p2;
+        let dot;
+        let rad;
+        for(let playerID of Object.keys(data.players)) {
+            
+            if (playerID !== socket.id){
+                p2 = data.players[playerID];
+                rad = Math.sqrt((p.pos.x - p2.pos.x)**2 + (p.pos.y - p2.pos.y)**2)
+                if (rad <= 60){
+
+        // Get the the player from the player's id
+    
+    dot = ((p.vel.xv - p2.vel.xv) * (p.pos.x - p2.pos.x)) + ((p.vel.yv - p2.vel.yv) * (p.pos.y - p2.pos.y))
+    let temp1 = ((dot / rad**2)*(p2.pos.x-p.pos.x));
+    let temp2 = ((dot / rad**2)*(p2.pos.y-p.pos.y));
+    p2.vel.xv = ((dot / rad**2)*(p.pos.x-p2.pos.x));
+    p2.vel.yv = ((dot / rad**2)*(p.pos.y-p2.pos.y));
+    p.vel.xv = temp1;
+    p.vel.yv = temp2;
+                }
+            
+            }
+
+        }
+      
+
+
+        
+         p.vel.yv *= 0.99;
+        p.vel.xv *= 0.99;
+        p.pos.y += p.vel.yv;
+        p.pos.x += p.vel.xv;
+    
+    if (p.pos.x > width - p.size) {
+            p.pos.x = width - p.size-1;
+        p.vel.xv *= -1
+        }
+        if(p.pos.x < p.size) {
+            p.pos.x = p.size;
+            p.vel.xv *= -1
+        }
+        if(p.pos.y > height - p.size) {
+            p.pos.y = height - p.size-1;
+            p.vel.yv *= -1
+        }
+        if(p.pos.y < p.size) {
+            p.pos.y = p.size;
+            p.vel.yv *= -1
+    }
+      
+    }
+    
+            socket.emit('update', data);
+            }, 0005);
+    
+  // Movement event
+  socket.on("move", key => {
+    // Get the current player
+    let player = data.players[socket.id];
+    // Check which movement key was pressed, and move accordingly
+    if(key == W) {
+        player.vel.yv -= 0.05;
+    }
+    if(key == A) {
+        player.vel.xv -= 0.05;
+    }
+    if(key == S) {
+        player.vel.yv += 0.05;
+    }
+    if(key == D) {
+        player.vel.xv += 0.05;
+    }
+    
+
+    // Check if player is touching the boundry, if so stop them from moving past it
+    if (player.pos.x > width - player.size) {
+        player.pos.x = width - player.size;
+    }
+    if(player.pos.x < 0) {
+        player.pos.x = 0;
+    }
+    if(player.pos.y > height - player.size) {
+        player.pos.y = height - player.size;
+    }
+    if(player.pos.y < 0) {
+        player.pos.y = 0;
+    }
+  });
+});
+
+
+
+http.listen(4141);
---
+++ script.js
@@ -0,0 +1,85 @@
+const socket = io();
+
+/* Setup */
+let canvas = document.getElementById('myCanvas');
+let ctx = canvas.getContext('2d');
+
+/* Update & Events */
+// Create the movement keys
+const W = 87;
+const A = 65;
+const S = 83;
+const D = 68;
+
+let up = 0;
+let down = 0;
+let left = 0;
+let right = 0;
+
+// Get the update from the server
+socket.on("update", data => {
+    p = data.players[socket.id];
+    
+    if (up == 1){
+            socket.emit('move', W, p)
+        }
+        if (left == 1){
+            socket.emit('move', A, p)
+        }
+        if (down == 1){
+            socket.emit('move', S, p)
+        }
+        if (right == 1){
+            socket.emit('move', D, p)
+        }
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // Draw each player
+  for(let playerID of Object.keys(data.players)) {
+    // Get the the player from the player's id
+    let player = data.players[playerID];
+
+    // Draw the player
+      ctx.beginPath();
+    ctx.arc(player.pos.x, player.pos.y, player.size, 0, Math.PI*2);
+    ctx.fillStyle = `rgb(
+        ${player.color.r},
+        ${player.color.g},
+        ${player.color.b})`;
+    ctx.fill();
+    ctx.stroke();
+    ctx.closePath();
+
+  }
+});
+
+//receive input to move player
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'w') {
+      up = 1;
+  }
+  if (event.key === 'a') {
+      left = 1;
+  }
+  if (event.key === 's') {
+      down = 1;
+  }
+  if (event.key === 'd') {
+      right = 1;
+  }
+});
+
+document.addEventListener('keyup', function (event) {
+  if (event.key === 'w') {
+      up = 0;
+  }
+  if (event.key === 'a') {
+      left = 0;
+  }
+  if (event.key === 's') {
+      down = 0;
+  }
+  if (event.key === 'd') {
+      right = 0;
+  }
+});
+
